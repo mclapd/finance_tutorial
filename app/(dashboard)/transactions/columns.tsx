e41148e3ed391b3e0e1cbd5c6ec7aa5af54bf60c
one_punch_man <ckim@banlaw.com>
2024-05-31 22:02:44 +1000

    Transactions CSV import

--- app/(dashboard)/transactions/columns.tsx
+++ app/(dashboard)/transactions/columns.tsx
@@ -57,7 +57,7 @@ export const columns: ColumnDef<ResponseType>[] = [
     cell: ({ row }) => {
       const date = row.getValue("date") as Date;
 
-      return <span>{format(date, "dd-MM-yyyy")}</span>;
+      return <span>{format(date, "yyyy-MM-dd")}</span>;
     },
   },
   {
