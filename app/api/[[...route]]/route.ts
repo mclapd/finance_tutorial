9b194e639788262ea81fcf746b11af818dd2584b
one_punch_man <ckim@banlaw.com>
2024-06-03 15:59:46 +1000

    Summary API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,7 +4,7 @@ import { handle } from "hono/vercel";
 import accounts from "./accounts";
 import categories from "./categories";
 import transactions from "./transactions";
-import { HTTPException } from "hono/http-exception";
+import summary from "./summary";
 
 export const runtime = "edge";
 
@@ -13,7 +13,8 @@ const app = new Hono().basePath("/api");
 const rountes = app
   .route("/accounts", accounts)
   .route("/categories", categories)
-  .route("/transactions", transactions);
+  .route("/transactions", transactions)
+  .route("/summary", summary);
 
 export const GET = handle(app);
 export const POST = handle(app);
